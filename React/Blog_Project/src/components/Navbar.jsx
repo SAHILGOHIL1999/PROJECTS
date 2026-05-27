0e5d9c6ebbdd2e615478902aaada45a5de900c15
SAHILGOHIL1999 <sahil19160@gmail.com>
2026-05-27 13:52:43 +0530

    file update

--- React/Blog_Project/src/components/Navbar.jsx
+++ React/Blog_Project/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <nav className="navbar">
-      <Link to="/" className="navbar-logo">📘 DocBlog</Link>
+      <Link to="/" className="navbar-logo">📘 Blog</Link>
       <div className="navbar-links">
         <Link to="/" className="navbar-link">Home</Link>
         <Link to="/blogs" className="navbar-link">Blog</Link>
